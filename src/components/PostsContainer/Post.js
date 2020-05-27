f82dfda526d40226d2866f0172e67590c00bea8e
Sarahmarie D Hollenstein <sholle7@gmail.com>
2020-05-26 19:03:10 -0500

    :tada: Project completed. Will work on stretch another time

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,29 +9,31 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-
+  const [likes, setLikes] = useState(props.post.likes)
   return (
-    <div className="post-border">
-      <PostHeader
-        username={props.post.username}
-        thumbnailUrl={
+		<div className="post-border">
+			<PostHeader
+				username={props.post.username}
+				thumbnailUrl={
           props.post.thumbnailUrl
         }
-      />
-      <div className="post-image-wrapper">
-        <img
-          alt="post thumbnail"
-          className="post-image"
-          src={props.post.imageUrl}
-        />
-      </div>
-      <LikeSection />
-      <CommentSection
-        postId={props.post.imageUrl}
+			/>
+			<div className="post-image-wrapper">
+				<img
+					alt="post thumbnail"
+					className="post-image"
+					src={props.post.imageUrl}
+				/>
+			</div>
+			<LikeSection likes={likes}  setLikes={setLikes}/>
+			<CommentSection
+				postId={props.post.imageUrl}
         comments={props.post.comments}
-      />
-    </div>
-  );
+        time={props.post.timestamp}
+			/>
+  
+		</div>
+	);
 };
 
 export default Post;
